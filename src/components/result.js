8970575b789ddce65b11a79df0d07f5c79106c63
priyanka dharamsoth <priyanka.d17@iiits.in>
2022-04-05 13:23:26 +0530

    built a basic quiz app using fuctional and class component

--- src/components/result.js
+++ src/components/result.js
@@ -5,7 +5,6 @@ const Result = ({score, playAgain}) => {
     <div className='score-board'>
         <div className='score'>You scored {score}/5 correct answers</div>
         <button className='playBtn' onClick={playAgain}>PlayAgain</button>
-      
     </div>
   )
 }
